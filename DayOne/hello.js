36619e3d2731cc03ebaeb7078c1d3c020876589b
DaleWillemse <DaleWillemse000@gmail.com>
2022-08-10 15:23:41 +0200

    Day one Javascript

---
+++ DayOne/hello.js
@@ -0,0 +1,25 @@
+const hello = "Hello, World!";
+
+// Print "Hello, World" 2 times.
+for (let i = 0; i < 2; i++) {
+    console.log(hello);
+}
+
+
+// let... local variables.
+let message = "hi";
+// var... global variables.
+var messageTwo = "hey";
+// const 
+const messageThree = "hello";
+
+// typeof returns the data type of a given variable.
+console.log(typeof message);
+
+// Curly brackets represents a block of statements 
+// that go together.
+while (message === "hi") {
+    message = "Bye World!";
+}
+
+console.log(message);
