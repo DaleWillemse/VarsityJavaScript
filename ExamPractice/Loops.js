dc17a8d7f1567af87b532d2856f8ae6e8467cd0a
DaleWillemse <DaleWillemse000@gmail.com>
2022-09-11 22:40:15 +0200

    basic practice.

--- ExamPractice/Loops.js
+++ ExamPractice/Loops.js
@@ -1,4 +1,11 @@
 // For loop
 for (let i = 0; i < 10; i++) {
+    // console.log(`Number ${i}`);
+}
+
+// While loop
+let i = 1;
+while (i < 4){
     console.log(`Number ${i}`);
+    i++;
 }
---
+++ ExamPractice/switch.js
@@ -0,0 +1,12 @@
+let choice = 1;
+
+switch (choice) {
+    case 1:
+        console.log(`Choice is equal to 1`);
+        break;
+    case 2:
+        console.log(`Choice is equal to 2`);
+        break;
+    default:
+        break;
+}
