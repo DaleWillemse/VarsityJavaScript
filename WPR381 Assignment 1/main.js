1ee0e519d2bcc9ace6a24c0c0a02421250814de0
Dale Willemse <DaleWillemse000@gmail.com>
2023-05-23 17:08:08 +0200

    error handling

--- WPR381 Assignment 1/main.js
+++ WPR381 Assignment 1/main.js
@@ -11,21 +11,52 @@ rl.question(
   "\nPlease enter the shape you want to draw: \n1. Triangle\n2. Square\n3. Diamond\n4. Exit\n\nEnter your choice : ",
   (shape) => {
     shape = parseInt(shape);
-    if (shape == 1) {
-      drawShape("triangle", shape);
-    } else if (shape == 2) {
-      drawShape("square", shape);
-    } else if (shape == 3) {
-      drawShape("diamond", shape);
+    if (shape > 0 && shape < 4) {
+      chooseShape(shape);
     } else if (shape == 4) {
-      console.log("Exiting...");
       rl.close();
     } else {
       console.log("Invalid input");
-      rl.close();
+      rl.setPrompt("Invalid input. Please enter a valid input : ");
+      rl.prompt();
+      rl.on("line", (shape) => {
+        // Error handling for invalid input.
+        shape = parseInt(shape);
+        if (shape > 0 && shape < 4) {
+          chooseShape(shape);
+        } else if (shape == 4) {
+          rl.close();
+        } else {
+          console.log("Invalid input");
+          rl.setPrompt(
+            "Invalid input. Please enter a valid input (1, 2, 3, or 4) : "
+          );
+          rl.prompt();
+        }
+      });
     }
   }
 );
+
+rl.on("close", () => {
+  console.log("\nExiting...");
+  process.exit(0);
+});
+
+// Function that takes shape choice as input and calls drawShape function.
+function chooseShape(shape) {
+  if (shape == 1) {
+    drawShape("triangle", shape);
+  } else if (shape == 2) {
+    drawShape("square", shape);
+  } else if (shape == 3) {
+    drawShape("diamond", shape);
+  } else if (shape == 4) {
+    rl.close();
+  } else {
+    console.log("Invalid input");
+  }
+}
 
 // Function that takes shape name and shape choice as input and draws the shape to the console.
 function drawShape(shapeName, shape) {
