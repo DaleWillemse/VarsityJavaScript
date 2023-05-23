855f723acfe496bad36e1b677e25e457d4193c1d
Dale Willemse <DaleWillemse000@gmail.com>
2023-05-23 21:58:55 +0200

    return shape name for displaying

--- WPR381 Assignment 1/menuModule.js
+++ WPR381 Assignment 1/menuModule.js
@@ -1,10 +1,26 @@
 // Menu module for choosing which shape to draw.
 
-exports.displayMenu = function () {
+function displayMenu() {
   console.clear();
   console.log(
     "Draw some shapes!\n--------------\n1. Triangle\n2. Square\n3. Diamond\n4. Exit\n"
   );
 
   return "Which shape do you want to draw: ";
+}
+
+function shapeName(choice) {
+  if (choice == 1) {
+    shapeName = "Triangle";
+  } else if (choice == 2) {
+    shapeName = "Square";
+  } else if (choice == 3) {
+    shapeName = "Diamond";
+  }
+  return shapeName;
+}
+
+module.exports = {
+  displayMenu: displayMenu,
+  shapeName: shapeName,
 };
