10e2f8ef438440664b02331a99947c14dd8c5439
Dale Willemse <DaleWillemse000@gmail.com>
2023-05-23 21:39:07 +0200

    error handling

--- WPR381 Assignment 1/main.js
+++ WPR381 Assignment 1/main.js
@@ -1,80 +1,82 @@
 const readline = require("readline");
 const shapes = require("./shapesModule.js");
+const menu = require("./menuModule.js");
 
 const rl = readline.createInterface({
   input: process.stdin,
   output: process.stdout,
 });
 
-// Take input from user for shape choice.
-rl.question(
-  "\nPlease enter the shape you want to draw: \n1. Triangle\n2. Square\n3. Diamond\n4. Exit\n\nEnter your choice : ",
-  (shape) => {
-    shape = parseInt(shape);
-    if (shape > 0 && shape < 4) {
-      chooseShape(shape);
-    } else if (shape == 4) {
-      rl.close();
-    } else {
-      console.log("Invalid input");
-      rl.setPrompt("Invalid input. Please enter a valid input : ");
-      rl.prompt();
-      rl.on("line", (shape) => {
-        // Error handling for invalid input.
-        shape = parseInt(shape);
-        if (shape > 0 && shape < 4) {
-          chooseShape(shape);
-        } else if (shape == 4) {
-          rl.close();
-        } else {
-          console.log("Invalid input");
-          rl.setPrompt(
-            "Invalid input. Please enter a valid input (1, 2, 3, or 4) : "
-          );
-          rl.prompt();
-        }
-      });
-    }
+rl.question(menu.displayMenu(), (shape) => {
+  shape = parseInt(shape);
+  if (shape > 0 && shape < 4) {
+    drawShape(shape);
+  } else if (shape == 4) {
+    rl.close();
+  } else {
+    menu.displayMenu();
+    rl.setPrompt("Invalid input. Please enter a valid input : ");
+    rl.prompt();
+    rl.on("line", (shape) => {
+      shape = parseInt(shape);
+      if (shape > 0 && shape < 4) {
+        drawShape(shape);
+      } else if (shape == 4) {
+        rl.close();
+      } else {
+        menu.displayMenu();
+        rl.setPrompt(
+          "Invalid input. Please enter a valid input (1, 2, 3, or 4) : "
+        );
+        rl.prompt();
+      }
+    });
   }
-);
+});
 
 rl.on("close", () => {
   console.log("\nExiting...");
   process.exit(0);
 });
 
-// Function that takes shape choice as input and calls drawShape function.
-function chooseShape(shape) {
-  if (shape == 1) {
-    drawShape("triangle", shape);
-  } else if (shape == 2) {
-    drawShape("square", shape);
-  } else if (shape == 3) {
-    drawShape("diamond", shape);
-  } else if (shape == 4) {
-    rl.close();
-  } else {
-    console.log("Invalid input");
-  }
-}
-
 // Function that takes shape name and shape choice as input and draws the shape to the console.
-function drawShape(shapeName, shape) {
-  rl.question(`\nPlease enter the ${shapeName} dimension : `, (n) => {
+function drawShape(shape) {
+  rl.question(`\nPlease enter the dimension : `, (n) => {
     n = parseInt(n);
-    if (shape == 1) {
-      console.log("\nTriangle Pattern");
-      shapes.drawTriangle(n);
-    } else if (shape == 2) {
-      console.log("\nSquare Pattern");
-      shapes.drawSquare(n);
-    } else if (shape == 3) {
-      console.log("\nDiamond Pattern");
-      shapes.drawDiamond(n);
+    if (n > 0 && Number.isInteger(n)) {
+      if (shape == 1) {
+        console.log("\nTriangle Pattern");
+        shapes.drawTriangle(n);
+      } else if (shape == 2) {
+        console.log("\nSquare Pattern");
+        shapes.drawSquare(n);
+      } else if (shape == 3) {
+        console.log("\nDiamond Pattern");
+        shapes.drawDiamond(n);
+      }
     } else {
-      console.log("Invalid input");
-      rl.close();
+      rl.setPrompt("Invalid input. Please enter a valid input : ");
+      rl.prompt();
+      rl.on("line", (n) => {
+        n = parseInt(n);
+        if (n > 0 && Number.isInteger(n)) {
+          if (shape == 1) {
+            console.log("\nTriangle Pattern");
+            shapes.drawTriangle(n);
+          } else if (shape == 2) {
+            console.log("\nSquare Pattern");
+            shapes.drawSquare(n);
+          } else if (shape == 3) {
+            console.log("\nDiamond Pattern");
+            shapes.drawDiamond(n);
+          }
+        } else {
+          rl.setPrompt(
+            "Invalid input. Please enter a valid input (a positive integer) : "
+          );
+          rl.prompt();
+        }
+      });
     }
-    rl.close();
   });
 }
