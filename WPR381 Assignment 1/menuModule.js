4a7612f378e1eda76678dd5091d9e4d21e1c3556
Dale Willemse <DaleWillemse000@gmail.com>
2023-05-23 21:38:38 +0200

    Creating more modules

---
+++ WPR381 Assignment 1/menuModule.js
@@ -0,0 +1,8 @@
+// Menu module for choosing which shape to draw.
+
+exports.displayMenu = function () {
+  console.clear();
+  console.log(
+    "Draw some shapes!\n--------------\n1. Triangle\n2. Square\n3. Diamond\n4. Exit\nWhich shape do you want to draw:"
+  );
+};
